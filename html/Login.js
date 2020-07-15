8aa75c5d8228171b067227aa88af47bd435125c8
Atulmalhotra27 <63745649+Atulmalhotra27@users.noreply.github.com>
2020-07-15 22:46:44 +0530

    textbox empty

--- html/Login.js
+++ html/Login.js
@@ -4,4 +4,6 @@ function myFunction()
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
   console.log(username,password);
+  document.getElementById("username").value="";
+  document.getElementById("password").value="";
 }
