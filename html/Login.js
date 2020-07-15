f9cb1bf3349501688ef3f19d49b70099d2886949
Atulmalhotra27 <63745649+Atulmalhotra27@users.noreply.github.com>
2020-07-15 22:39:42 +0530

    Login html page

---
+++ html/Login.js
@@ -0,0 +1,7 @@
+
+function myFunction() 
+{
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  console.log(username,password);
+}
